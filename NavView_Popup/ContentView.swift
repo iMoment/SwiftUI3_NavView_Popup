ebe1cc7a05593fba7c3883bb0492f3be1233f2a7
Stanley Pan <imoment.developer@gmail.com>
2022-01-17 14:32:41 -0500

    Initial Commit

---
+++ NavView_Popup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavView_Popup
+//
+//  Created by Stanley Pan on 2022/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavView_Popup/NavView_PopupApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavView_PopupApp.swift
+//  NavView_Popup
+//
+//  Created by Stanley Pan on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct NavView_PopupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
